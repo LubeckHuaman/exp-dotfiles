3e6fa3bfb4152fd67e99ed254cca93909ecb9eee
lubeck a. <lubeck05@gmail.com>
2026-06-01 23:01:07 -0500

    nvim: back to mocha (more reddish)

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -485,7 +485,7 @@ return {
     priority = 1000,
     config = function()
       require('catppuccin').setup {
-        flavour = 'macchiato',
+        flavour = 'mocha',
         transparent_background = false,
       }
       vim.cmd.colorscheme 'catppuccin'
