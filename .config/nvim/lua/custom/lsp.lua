aa999fd9f2f48ea015f320989bd12d51ea3bc079
lubeck a. <lubeck05@gmail.com>
2026-06-04 00:20:43 -0500

    fix: remove CursorHoldI from document_highlight

--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -26,7 +26,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
       local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      vim.api.nvim_create_autocmd('CursorHold', {
         buffer = event.buf,
         group = highlight_augroup,
         callback = vim.lsp.buf.document_highlight,
