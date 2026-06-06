b1060d43cd75101902d00a93b39d6f28794ff310
lubeck a. <lubeck05@gmail.com>
2026-06-05 23:21:23 -0500

    nvim: catppuccin macchiato

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -498,7 +498,7 @@ return {
     priority = 1000,
     config = function()
       require('catppuccin').setup {
-        flavour = 'mocha',
+        flavour = 'macchiato',
         transparent_background = false,
       }
       vim.cmd.colorscheme 'catppuccin'
