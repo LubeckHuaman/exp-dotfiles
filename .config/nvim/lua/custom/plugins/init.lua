3615d2a249cf429c83db13a527263bda9dce4df2
lubeck a. <lubeck05@gmail.com>
2026-06-20 19:46:06 -0500

    Switch to gruvbox-material with transparency

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -444,15 +444,13 @@ return {
   -- },
 
   {
-    'ellisonleao/gruvbox.nvim',
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
     config = function()
-      require('gruvbox').setup {
-        contrast = 'hard',
-        terminal_colors = true,
-        transparent_mode = true,
-      }
-      vim.cmd.colorscheme 'gruvbox'
+      vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_transparent_background = 1
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 
