ac1fb50e82bf7a65cab40cad9dc30f7b19820b97
lubeck a. <lubeck05@gmail.com>
2026-06-21 00:01:26 -0500

    Switch to nightfox carbonfox with transparent

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -447,16 +447,30 @@ return {
   --   end,
   -- },
 
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       flavour = 'macchiato',
+  --       transparent_background = true,
+  --     }
+  --     vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  -- },
+
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'EdenEast/nightfox.nvim',
+    lazy = false,
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        flavour = 'macchiato',
-        transparent_background = true,
-      }
-      vim.cmd.colorscheme 'catppuccin'
+      require('nightfox').setup({
+        options = {
+          transparent = true,
+        },
+      })
+      vim.cmd.colorscheme 'carbonfox'
     end,
   },
 
